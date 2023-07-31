95c60deff886420a1d8e5160a3dad0d473ad1784
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-07-31 15:54:39 +0200

    fixed the router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'components/App/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/iakivpekarskyi">
       <App />
     </BrowserRouter>
   </React.StrictMode>
