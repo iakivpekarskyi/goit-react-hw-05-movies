4517ef231762563ce56d7f3f7770380714a8b372
iakivpekarskyi <108753193+iakivpekarskyi@users.noreply.github.com>
2023-08-16 19:16:38 +0000

    Deploying to gh-pages from @ iakivpekarskyi/goit-react-hw-05-movies@a1063f5c64178ee53b83cc6f7647d646a5cdefbe 🚀

---
+++ static/js/186.1db18832.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h3",{children:"REVIEWS Page"}),";"]})}}}]);
+//# sourceMappingURL=186.1db18832.chunk.js.map
---
+++ static/js/805.cdf0ec1a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);var s=r(184);t.default=function(){return(0,s.jsxs)("main",{style:{display:"grid",placeItems:"center"},children:[(0,s.jsx)("h1",{children:"Sorry, wrong URL."}),(0,s.jsx)("img",{src:"https://i.gifer.com/7VE.gif",alt:"",style:{width:"100%",height:"auto"}}),(0,s.jsx)("p",{})]})}}}]);
+//# sourceMappingURL=805.cdf0ec1a.chunk.js.map
